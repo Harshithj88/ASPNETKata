d48f210eb78f7869aa9c065e9c664f36069dbcb1
Harshith Julapelli <harsh.julapelli@gmail.com>
2017-11-12 23:11:44 -0600

    Added class library and referenced it to the main class.

---
+++ ASPNETKata.Shared/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNETKata.Shared
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product prod);
+        void InsertProduct(Product prod);
+    }
+}
